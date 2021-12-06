360bae82459d5b6d60540a54ec60938b0a2b2b15
wstyczen <57729793+wstyczen@users.noreply.github.com>
2021-12-06 21:56:55 +0100

    Day6

--- Day6.py
+++ Day6.py
@@ -1,20 +1,5 @@
 from re import compile, findall
 
-def tick(fish):
-    fish = [aFish - 1 for aFish in fish]
-    new = fish.count(-1)
-    fish = [6 if aFish == -1 else aFish for aFish in fish]
-    for i in range(new):
-        fish.append(8)
-    return fish
-
-def part1(fish, days):
-    day = 0
-    while day < days:
-        fish = tick(fish)
-        day += 1
-    return len(fish)
-
 def getDictFish(fish):
     dict = {}
     for i in range(9):
@@ -23,7 +8,7 @@ def getDictFish(fish):
         dict[aFish] += 1
     return dict
 
-def part2(fish, days):
+def getFishAfter(fish, days):
     dictFish = getDictFish(fish)
     day = 0
     while day < days:
@@ -39,5 +24,5 @@ def part2(fish, days):
 with open("Day6.txt") as f:
     pattern = compile("\\d+")
     fish = [int(fish) for fish in findall(pattern, f.readline())]
-    print(part2(fish, 256))
+    print(getFishAfter(fish, 256))
     # print(fish)
